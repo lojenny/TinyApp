89e7573d6a9f8cbbbd4de0068433c50bfd7356ac
lojenny <jennychingyinlo@gmail.com>
2017-09-13 18:53:20 +0000

    added delete option

--- express_server.js
+++ express_server.js
@@ -1,11 +1,16 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
+const bodyParser = require("body-parser"); //middleware
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
 
 app.set("view engine", "ejs");
-
-const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+
 
 function generateRandomString() {
   var text = "";
@@ -15,30 +20,29 @@ function generateRandomString() {
   return text;
   };
 
-var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-
-app.post("/urls", (req, res) => {
-  let shortURL = generateRandomString();
-  urlDatabase[shortURL]=req.body.longURL;
-  res.redirect(`http://localhost:8080/urls/${shortURL}`);
-});
-
 app.get("/u/:shortURL", (req, res) => {
   if (urlDatabase[req.params.shortURL] === undefined){
-    res.redirect(404, "/urls/new"); //redirect to an error page with status code//
+    res.redirect(404, "/urls/new"); 
   }else {
   let longURL = urlDatabase[req.params.shortURL];
   res.status(302)
   res.redirect(longURL);
   }
+});
+
+app.post("/urls", (req, res) => {
+  let shortURL = generateRandomString();
+  while (urlDatabase[shortURL] !== undefined){ 
+    shortURL = generateRandomString();
+  }
+  urlDatabase[shortURL]=req.body.longURL; //add to short and long URL key pair to database
+  res.status(302);
+  res.redirect(`http://localhost:8080/urls/${shortURL}`);
 });
 
 app.get("/urls", (req, res) => {
@@ -58,17 +62,30 @@ app.get("/urls/:id", (req, res) => {
   }
 });
 
-app.get("/", (req, res) => {
-  res.end("Hello!");
+app.post("/urls/:id/delete", (req, res) => {
+  let currKey = req.params.id;
+  delete urlDatabase[currKey]; //add to short and long URL key pair to database
+  res.redirect('/urls');
 });
 
-app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
-  });
-
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
+app.post("/urls/:id", (req, res) => {
+  let currKey = req.params.id;
+  let newlongURL = req.body.longURL;
+  urlDatabase[currKey]= newlongURL;
+  res.redirect('/urls');
 });
+
+// app.get("/", (req, res) => {
+//   res.end("Hello!");
+// });
+
+// app.get("/urls.json", (req, res) => {
+//     res.json(urlDatabase);
+//   });
+
+// app.get("/hello", (req, res) => {
+//   res.end("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
